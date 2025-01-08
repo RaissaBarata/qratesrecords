54a22f839f9d6042338390e6dcb4b9cef9f85278
RaissaBarata <barata.raissa@gmail.com>
2025-01-08 01:37:55 -0300

    hero-section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component'; // Import do HeroSectionComponent
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeaderComponent], // Adicione aqui!
+  imports: [HeaderComponent, HeroSectionComponent], // Certifique-se de incluir o HeroSectionComponent aqui
 })
 export class AppComponent {
   title = 'qratesrecords';
