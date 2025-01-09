1f031c9c3887d2262c593bedc36ebc601932299d
RaissaBarata <barata.raissa@gmail.com>
2025-01-09 18:38:56 -0300

    hero section resp + animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { HeroSectionComponent } from './components/hero-section/hero-section.component'; // Import do HeroSectionComponent
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeaderComponent, HeroSectionComponent], // Certifique-se de incluir o HeroSectionComponent aqui
+  imports: [HeaderComponent, HeroSectionComponent],
 })
 export class AppComponent {
   title = 'qratesrecords';
--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './hero-section.component.html',
-  styleUrl: './hero-section.component.css'
+  styleUrls: ['./hero-section.component.css'],
+  animations: [
+    // Animação para o título (surgir de baixo)
+    trigger('fadeInUp', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(20px)' }),
+        animate(
+          '3s ease-out',
+          style({ opacity: 1, transform: 'translateY(0)' })
+        ),
+      ]),
+    ]),
+  ],
 })
-export class HeroSectionComponent {
+export class HeroSectionComponent implements OnInit {
+  description =
+    'Full-service pressing, a global retail network, customer support — and you keep up to 85% of profits.';
+  displayedText: string = '';
+  currentIndex: number = 0;
+  typingSpeed: number = 50;
 
+  ngOnInit() {
+    this.typeText();
+  }
+
+  private typeText() {
+    if (this.currentIndex < this.description.length) {
+      this.displayedText += this.description[this.currentIndex];
+      this.currentIndex++;
+
+      setTimeout(() => this.typeText(), this.typingSpeed);
+    }
+  }
 }
